3a789ffd3967758b77e0049b7898ad079e8a5055
Orla Sonvico <osonvico11@gmail.com>
2015-12-29 14:01:37 +0000

    Initial project push

---
+++ NYResO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYResO
+//
+//  Created by ResDiary on 29/12/2015.
+//  Copyright © 2015 ResDiary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYResO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYResO
+//
+//  Created by ResDiary on 29/12/2015.
+//  Copyright © 2015 ResDiary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
